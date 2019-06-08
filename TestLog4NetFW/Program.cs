4462b7d095836541064f534e4cf08c68160e7908
lionel-git <lionel.desorme@free.fr>
2019-06-08 13:13:00 +0200

    Add test with Fw

---
+++ TestLog4NetFW/Program.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+using log4net.Appender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLog4NetFW
+{
+    class Program
+    {
+
+        public static void TestChangeFile(ILog logger)
+        {
+            var ret = logger.Logger.Repository.GetAppenders();
+            FileAppender f = ret[0] as FileAppender;
+            f.File = @"c:\tmp\titi2.log";
+            f.ActivateOptions();
+            logger.Info("titi");
+        }
+
+
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger("zozo");
+            logger.Info("Test");
+
+            TestChangeFile(logger);
+        }
+    }
+}
