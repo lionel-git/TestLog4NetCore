8c93e709d0f7dbebefec24c404d525c320d32bd1
lionel-git <lionel.desorme@free.fr>
2019-06-08 11:55:05 +0200

    Initial commit

---
+++ TestLog4NetCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using log4net;
+using log4net.Config;
+
+namespace TestLog4NetCore
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            var logger = LogManager.GetLogger(typeof(Program));
+            logger.Error("Hello World!");
+        }
+    }
+}
