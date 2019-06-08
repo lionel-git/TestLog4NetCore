df9799e9c67f92c917afbd702a0f0f0c7429057e
lionel-git <lionel.desorme@free.fr>
2019-06-08 12:10:19 +0200

    Test FileName as global var + change filename

--- TestLog4NetCore/Program.cs
+++ TestLog4NetCore/Program.cs
@@ -3,18 +3,45 @@ using System.IO;
 using System.Reflection;
 using System.Xml;
 using log4net;
+using log4net.Appender;
 using log4net.Config;
 
 namespace TestLog4NetCore
 {
     class Program
     {
+        public static void TestChangeFile(ILog logger)
+        {
+            var ret= logger.Logger.Repository.GetAppenders();
+            FileAppender f = ret[0] as FileAppender;
+            f.File = @"c:\tmp\titi.log";
+            f.ActivateOptions();
+            logger.Info("zizi");          
+        }
+
+
         public static void Main()
         {
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            log4net.GlobalContext.Properties["LogFileName"] = @"c:\tmp\toto0.log"; //log file path 
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+
             var logger = LogManager.GetLogger(typeof(Program));
             logger.Error("Hello World!");
+
+            log4net.GlobalContext.Properties["LogFileName"] = @"c:\tmp\toto2.log"; //log file path 
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            logger.Error("Hello World toto2");
+
+            log4net.GlobalContext.Properties["LogFileName"] = @"c:\tmp\toto3.log"; //log file path 
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            logger.Error("Hello World toto3");
+
+            TestChangeFile(logger);
+
+
         }
     }
 }
